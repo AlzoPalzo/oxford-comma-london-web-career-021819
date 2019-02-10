4eada8550cb69ac4b322fc73f6aab4065632c072
Ali Madni <ali555m@hotmail.com>
2019-02-10 16:41:10 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-
+  if array.length = 1
+    array
+  elsif array.length = 2
+    array.join(" and ")
+  else
+    small_array == array[0..arrayl.length - 2]
+    small_array.join(", ")
+    
+  end
 end
