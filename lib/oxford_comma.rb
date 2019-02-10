a9d4e6df4b46989a8ca83141b02c45e01dfcfe93
Ali Madni <ali555m@hotmail.com>
2019-02-10 16:43:21 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,7 +4,7 @@ def oxford_comma(array)
   elsif array.length = 2
     array.join(" and ")
   else
-    small_array == array[0..arrayl.length - 2]
+    small_array == array[0..array.length - 2]
     output = small_array.join(", ") + " and #{array.last}"
     output
   end
